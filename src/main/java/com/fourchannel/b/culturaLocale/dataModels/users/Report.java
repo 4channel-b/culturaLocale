3f19a712578e9320007e9c8f09d7114e1d27219d
test <test>
2023-12-28 19:33:57 +0100

    fixed compilation error because of old IContent reference

--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/users/Report.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/users/Report.java
@@ -1,6 +1,6 @@
 package com.fourchannel.b.culturaLocale.dataModels.users;
 
-import com.fourchannel.b.culturaLocale.dataModels.IContent;
+import com.fourchannel.b.culturaLocale.dataModels.Content;
 import com.fourchannel.b.culturaLocale.dataModels.users.IUser;
 
 import java.util.Date;
@@ -12,5 +12,5 @@ public class Report {
     private Date reportingDate;
     private int status;
     private String resolution;
-    private IContent content;
+    private Content content;
 }
