ad3f2af6cb2995593069817806f7ff0ebc9b5dac
Deputation <deputation2@gmail.com>
2024-01-09 21:23:49 +0000

    fixed compilation errors and reformatted a file

--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/Content.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/Content.java
@@ -1,6 +1,7 @@
 package com.fourchannel.b.culturaLocale.dataModels;
 
 import java.util.Date;
+import com.fourchannel.b.culturaLocale.dataModels.users.User;
 
 public abstract class Content {
     String name = null;
@@ -8,15 +9,36 @@ public abstract class Content {
     // GMT.
     Date creationDate = null;
     ImageSet images = null;
-    IUser creator = null;
+    User creator = null;
     ApprovalStatus status = null;
 
-    public  String getName() { return this.name; }
-    public  String getDescription() { return this.description; }
-    public  Date getCreationDate() { return this.creationDate; }
-    public  ImageSet getImages() { return this.images; }
-    public IUser getCreator() { return this.creator; }
-    public ApprovalStatus getStatus() { return this.status; }
-    public void setStatus(ApprovalStatus status) { this.status = status; }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public ImageSet getImages() {
+        return this.images;
+    }
+
+    public User getCreator() {
+        return this.creator;
+    }
+
+    public ApprovalStatus getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(ApprovalStatus status) {
+        this.status = status;
+    }
+
     public abstract String getContentType(); // Needed to discern content types in class agnostic lists
 }
--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/Report.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/Report.java
@@ -2,9 +2,11 @@ package com.fourchannel.b.culturaLocale.dataModels;
 
 import java.util.Date;
 
+import com.fourchannel.b.culturaLocale.dataModels.users.User;
+
 public class Report {
     private String ID;
-    private IUser reporter;
+    private User reporter;
     private String description;
     private Date reportingDate;
     private int status;
