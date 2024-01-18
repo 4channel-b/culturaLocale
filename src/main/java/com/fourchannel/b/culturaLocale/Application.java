6160d8a069659982d72fceb3e1cf6a099f75597f
Damiano Buzzo <buzzodamiano02@gmail.com>
2024-01-18 22:15:24 +0100

    works maybe better

--- src/main/java/com/fourchannel/b/culturaLocale/Application.java
+++ src/main/java/com/fourchannel/b/culturaLocale/Application.java
@@ -1,11 +1,12 @@
 package com.fourchannel.b.culturaLocale;
 
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })  //momentaneamente bloccata la ricerca di un datasource
+@SpringBootApplication  //momentaneamente bloccata la ricerca di un datasource
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/Report.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/Report.java
@@ -5,13 +5,26 @@ import com.fourchannel.b.culturaLocale.dataModels.users.User;
 import java.util.Date;
 
 import com.fourchannel.b.culturaLocale.dataModels.users.User;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name="report")
 public class Report {
-    private String ID;
+    @Id
+    private Long ID;
+    @ManyToOne
     private User reporter;
     private String description;
     private Date reportingDate;
     private int status;
     private String resolution;
-    private Content content;
+    //dovrebbero essere content
+    @ManyToOne
+    private PointOfInterest content;
 }
