373754c82aaa6b76e3709aa16ae883ed49ed8294
Deputation <deputation2@gmail.com>
2024-01-20 20:59:15 +0100

    tagged repositories that were missing the annotation

--- src/main/java/com/fourchannel/b/culturaLocale/repositories/ReportRepository.java
+++ src/main/java/com/fourchannel/b/culturaLocale/repositories/ReportRepository.java
@@ -2,7 +2,9 @@ package com.fourchannel.b.culturaLocale.repositories;
 
 import com.fourchannel.b.culturaLocale.dataModels.Report;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends CrudRepository<Report,Long> {
 
 }
--- src/main/java/com/fourchannel/b/culturaLocale/repositories/TownHallRoleRepository.java
+++ src/main/java/com/fourchannel/b/culturaLocale/repositories/TownHallRoleRepository.java
@@ -2,6 +2,8 @@ package com.fourchannel.b.culturaLocale.repositories;
 
 import com.fourchannel.b.culturaLocale.dataModels.users.TownHallRoleUser;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TownHallRoleRepository extends CrudRepository<TownHallRoleUser,Long> {
 }
