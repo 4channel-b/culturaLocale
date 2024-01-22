a6288c84ae404762a1f47ccb315883e84453d3f6
Deputation <deputation2@gmail.com>
2024-01-22 20:02:27 +0100

    removed mapstruct

--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/DTOs/Mappers/UserMapper.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/DTOs/Mappers/UserMapper.java
@@ -4,12 +4,8 @@ import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserBasicDTO;
 import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserCreationRequestDTO;
 import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserSuspensionDTO;
 import com.fourchannel.b.culturaLocale.dataModels.users.User;
-import org.mapstruct.Builder;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(builder = @Builder(disableBuilder = false))
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
