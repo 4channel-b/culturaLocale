f54ce1b744824e32f8347c90054540dce7dd4a53
Deputation <deputation2@gmail.com>
2024-01-22 20:03:08 +0100

    simple singleton

--- src/main/java/com/fourchannel/b/culturaLocale/dataModels/DTOs/Mappers/UserMapper.java
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/DTOs/Mappers/UserMapper.java
@@ -4,10 +4,10 @@ import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserBasicDTO;
 import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserCreationRequestDTO;
 import com.fourchannel.b.culturaLocale.dataModels.DTOs.UserSuspensionDTO;
 import com.fourchannel.b.culturaLocale.dataModels.users.User;
-import org.mapstruct.factory.Mappers;
 
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    // TODO: Review this singleton
+    UserMapper INSTANCE = new UserMapperImpl();
 
     User userDtoToUser(UserCreationRequestDTO userDTO);
 }
