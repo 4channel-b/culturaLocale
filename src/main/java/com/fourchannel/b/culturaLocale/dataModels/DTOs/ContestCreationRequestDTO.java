19d210f7ab72e304b6fa5f1a3e5089265565db6b
Flaminaire000 <fereckavasil2002@gmail.com>
2024-01-25 17:30:14 +0100

    Created ContestCreationRequestDTO.java

---
+++ src/main/java/com/fourchannel/b/culturaLocale/dataModels/DTOs/ContestCreationRequestDTO.java
@@ -0,0 +1,21 @@
+package com.fourchannel.b.culturaLocale.dataModels.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ContestCreationRequestDTO {
+    private String name;
+    private String description;
+    private Date initialDate;
+    private Date endDate;
+    private String rules;
+    private String type;
+    private List<Long> contents;
+}
