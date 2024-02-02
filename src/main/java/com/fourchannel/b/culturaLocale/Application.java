b418117d4eed24b8c5d8e9e9dd3f3cd94d2cad07
Damiano Buzzo <buzzodamiano02@gmail.com>
2024-02-02 17:40:59 +0100

    added vpp file

--- src/main/java/com/fourchannel/b/culturaLocale/Application.java
+++ src/main/java/com/fourchannel/b/culturaLocale/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.fourchannel.b.culturaLocale.dataModels", "com.fourchannel.b.culturaLocale.dataModels.users"})
